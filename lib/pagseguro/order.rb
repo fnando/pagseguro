0ada9f19f36fa8be446948639aa0f5e8fd1896c5
Nando Vieira <fnando.vieira@gmail.com>
2009-11-25 10:35:01 -0200

    Closing issue #1

--- lib/pagseguro/order.rb
+++ lib/pagseguro/order.rb
@@ -2,32 +2,32 @@ module PagSeguro
   class Order
     # The list of products added to the order
     attr_accessor :products
-    
+
     # Define the shipping type.
     # Can be EN (PAC) or SD (Sedex)
     attr_accessor :shipping_type
-    
+
     def initialize(order_id = nil)
       reset!
       self.id = order_id
     end
-    
+
     # Set the order identifier. Should be a unique
     # value to identify this order on your own application
     def id=(identifier)
       @id = identifier
     end
-    
+
     # Get the order identifier
     def id
       @id
     end
-    
+
     # Remove all products from this order
     def reset!
       @products = []
     end
-    
+
     # Add a new product to the PagSeguro order
     # The allowed values are:
     # - weight (Optional. If float, will be multiplied by 1000g)
@@ -44,24 +44,30 @@ module PagSeguro
         :fees => nil,
         :quantity => 1
       }.merge(options)
-      
+
       # convert shipping to cents
-      options[:shipping] = (options[:shipping] * 100).to_i if options[:shipping].kind_of?(Float)
-      
+      options[:shipping] = convert_unit(options[:shipping], 100)
+
       # convert fees to cents
-      options[:fees] = (options[:fees] * 100).to_i if options[:fees].kind_of?(Float)
-      
+      options[:fees] = convert_unit(options[:fees], 100)
+
       # convert price to cents
-      options[:price] = (options[:price] * 100).to_i if options[:price].kind_of?(Float)
-      
+      options[:price] = convert_unit(options[:price], 100)
+
       # convert weight to grammes
-      options[:weight] = (options[:weight] * 1000).to_i if options[:weight].kind_of?(Float)
-      
+      options[:weight] = convert_unit(options[:weight], 1000)
+
       products.push(options)
     end
-    
+
     def add(options)
       self << options
     end
-  end  
+
+    private
+      def convert_unit(number, unit)
+        number = (number * unit).to_i unless number.nil? || number.kind_of?(Integer)
+        number
+      end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 RAILS_ENV = "test"
 
+require "rubygems"
 require "spec"
 require "fakeweb"
 require File.dirname(__FILE__) + "/../lib/pagseguro"
+
+require "bigdecimal"
 
 FakeWeb.allow_net_connect = false
 PagSeguro::ORIGINAL_CONFIG_FILE = PagSeguro::CONFIG_FILE
@@ -10,7 +13,7 @@ PagSeguro::CONFIG_FILE.gsub!(/^.*$/, File.dirname(__FILE__) + "/pagseguro.yml")
 
 class Object
   def self.unset_class(*args)
-    class_eval do 
+    class_eval do
       args.each do |klass|
         eval(klass) rescue nil
         remove_const(klass) if const_defined?(klass)
