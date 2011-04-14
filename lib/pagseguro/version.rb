3fbab51a18039fb21ee7b488cbeaac04083df127
Nando Vieira <fnando.vieira@gmail.com>
2011-04-14 07:12:03 -0300

    Bump up version

--- lib/pagseguro/version.rb
+++ lib/pagseguro/version.rb
@@ -2,7 +2,7 @@ module PagSeguro
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 9
-    STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
+    PATCH = 10
+    STRING = "#{MAJOR}.#{MINOR}.#{PATCH}.beta.0"
   end
 end
