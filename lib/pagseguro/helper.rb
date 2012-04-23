166dd2017449e7f0ac0a1d6bfedd225ca73c1727
Cirdes Henrique <cirdes@codemedia.com.br>
2012-04-23 13:59:50 -0300

    Changes to add backwards compatibility
    Modificações na maneira de adicionar a possibilidade de se passar
    opções para o submit_tag na view do cart para por exemplo: adicionar
    classes ao botão de forma que o css do twitter bootstrap possa funcionar

--- lib/pagseguro/helper.rb
+++ lib/pagseguro/helper.rb
@@ -1,5 +1,6 @@
 module PagSeguro::Helper
-  def pagseguro_form(order, value = "Pagar com PagSeguro", options = {})
-    render :partial => "pagseguro/pagseguro_form", :locals => {:value => value, :options => options, :order => order}
+  def pagseguro_form(order, options = {})
+    options.reverse_merge!(:submit => "Pagar com PagSeguro")
+    render :partial => "pagseguro/pagseguro_form", :locals => {:options => options, :order => order}
   end
 end
